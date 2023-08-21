8dc1b1c82b5ef11cb2f815eacf70f1a14674a94e
B1smuth <65298483+EliteGamerSiddhu@users.noreply.github.com>
2023-08-21 18:20:37 +0530

    199A Hexadecimals theorum rating 900

---
+++ 800 to 999/199A Hexadecimals theorm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int ques;
+    cin >> ques;
+    cout << 0 <<" " << 0 << " " << ques;
+    return 0;
+}
